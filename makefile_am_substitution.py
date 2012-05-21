eac69a1f79cf1dab471231be9f39c1101387c7d0
Ioann Volkov <volkov.ioann@gmail.com>
2012-05-21 16:24:15 +0400

    Now script finds everything that needs replacement

--- makefile_am_substitution.py
+++ makefile_am_substitution.py
@@ -1,22 +1,32 @@
 #!/usr/bin/env python2.7
 
-import os, string
+import os, string, re
 
-def tryDir(path, old, new):
+def scanDir(path):
+    mkfList = []
     fileNames = [path + "/Makefile.am", path + "/makefile.am"]
     for fileName in fileNames:
         if os.path.exists(fileName):
-            with open(fileName, "r") as f:
-                s = f.read()
-                s = string.replace(s, old, new)
-            with open(fileName, "w") as f:
-                f.write(s)
+            mkfList.append(fileName)
     l = os.listdir(path)
     for pth in l:
         fpth = path + "/" + pth
         if os.path.isdir(fpth):
-            tryDir(fpth, old, new)
+            mkfList.extend(scanDir(fpth))
+    return mkfList
+
+def needReplacement(mkf):
+    res = set()
+    with open(mkf, "r") as f:
+        s = f.read()
+    l = re.findall(r'\$\(\S+?\)', s)
+    for x in l:
+        res.add(x)
+    return res
 
 if __name__ == "__main__":
-    tryDir("/home/volkov/work/dev/ext/src/GraphicsMagick-1.3.14", "$(LIBMAGICK)", "magick/libGraphicsMagick.la")
-    tryDir("/home/volkov/work/dev/ext/src/GraphicsMagick-1.3.14", "$(LIBMAGICKPP)", "Magick++/lib/libGraphicsMagick++.la")
+    mkfList = scanDir("/home/volkov/work/dev/ext/src/GraphicsMagick-1.3.14")
+    nr = set()
+    for mkf in mkfList:
+        nr |= needReplacement(mkf)
+    print nr
